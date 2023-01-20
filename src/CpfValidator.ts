eab7f9723637b2212edbbd2007f527f611ca0a79
Nicolas Soares <nsosoares@gmail.com>
2023-01-20 16:32:52 -0300

    commit backup

--- src/CpfValidator.ts
+++ src/CpfValidator.ts
@@ -1,61 +1,52 @@
 // @ts-nocheck
-export function validate (str) {
 
-	if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 || str.length <= 14){
-
-                str=str
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");  
-    
-                if (!str.split("").every(c => c === str[0])) {
-                    try{  
-                        let     d1, d2;  
-                        let     dg1, dg2, rest;  
-                        let     digito;  
-                            let     nDigResult;  
-                        d1 = d2 = 0;  
-                        dg1 = dg2 = rest = 0;  
-                            
-                        for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                            // 	return false;
-                            // } else {
-    
-                                digito = parseInt(str.substring(nCount -1, nCount));  							
-                                d1 = d1 + ( 11 - nCount ) * digito;  
-                
-                                d2 = d2 + ( 12 - nCount ) * digito;  
-                            // }
-                        };  
-                            
-                        rest = (d1 % 11);  
-                
-                        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-                        d2 += 2 * dg1;  
-                        rest = (d2 % 11);  
-                        if (rest < 2)  
-                            dg2 = 0;  
-                        else  
-                            dg2 = 11 - rest;  
-                
-                            let nDigVerific = str.substring(str.length-2, str.length);  
-                        nDigResult = "" + dg1 + "" + dg2;  
-                        return nDigVerific == nDigResult;
-                    }catch (e){  
-                        console.error("Erro !"+e);  
-    
-                        return false;  
-                    }  
-                } else return false
-    
-            }else return false;
-        }
+const MINIMUM_AMOUNT_OF_CHARACTERS = 11;
 
 
-	} else return false;
-
+export function validate (cpf: string) {
+    const cleanCpf = clearCpf(cpf);
+    const checkDigits = extractDigits(cleanCpf);
+    if(!cpfIsValid(cleanCpf)) return false;
+    if(allDigitsTheSame(cleanCpf)) return false;
+    //TODO:Refatoras esse calculo
+    let resultadoPrimeiroDigito, resultadoSegundoDigito;  
+    let primeiroDigitoVerificador, segundoDigitoVerificador, resultadoRestoDaDivisao;  
+    let digito;  
+    resultadoPrimeiroDigito = resultadoSegundoDigito = 0;  
+    primeiroDigitoVerificador = segundoDigitoVerificador = resultadoRestoDaDivisao = 0;  
+    for (let nCount = 1; nCount < cleanCpf.length - 1; nCount++) {  
+        digito = parseInt(cleanCpf.substring(nCount - 1, nCount));  							
+        resultadoPrimeiroDigito = resultadoPrimeiroDigito + ( 11 - nCount ) * digito;  
+        resultadoSegundoDigito = resultadoSegundoDigito + ( 12 - nCount ) * digito;  
+    };  
+    resultadoRestoDaDivisao = (resultadoPrimeiroDigito % 11);  
+    primeiroDigitoVerificador = (resultadoRestoDaDivisao < 2) ? primeiroDigitoVerificador = 0 : 11 - resultadoRestoDaDivisao;  
+    resultadoSegundoDigito += 2 * primeiroDigitoVerificador;  
+    resultadoRestoDaDivisao = (resultadoSegundoDigito % 11);  
+    segundoDigitoVerificador = resultadoRestoDaDivisao < 2 ? 0 : 11 - resultadoRestoDaDivisao;
+    const VerifierDigitsResult = `${primeiroDigitoVerificador}${segundoDigitoVerificador}`;  
+    return checkDigits === VerifierDigitsResult;
 }
+
+
+function clearCpf(cpf: string) {
+    return cpf.replace(/\D/g, "");
+}
+
+function cpfIsValid(cleanCpf: string) {
+    return cleanCpf !== null && cleanCpf !== undefined && cleanCpf.length === MINIMUM_AMOUNT_OF_CHARACTERS;
+}
+
+function allDigitsTheSame(cleanCpf: string) {
+    const firstDigit = cleanCpf[0];
+    return cleanCpf.split("").every(digit => digit === firstDigit);
+}
+
+function extractDigits(cleanCpf: string) {
+    return cleanCpf.slice(9);
+}
+
+function calculateDigits(cleanCpf: string) {
+    let result = 0;
+}
+
