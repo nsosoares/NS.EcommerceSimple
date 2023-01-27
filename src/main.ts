0b8ab9deea875c53d9c936ac93af18e026265149
Nicolas Soares <nsosoares@gmail.com>
2023-01-26 22:21:33 -0300

    Create database connection

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
 import express from "express";
 import { validate } from "./cpfValidator";
+import pgp from "pg-promise";
 
 const app = express();
 const PORT = 3000;
 app.use(express.json());
 
-const products = [
-    {idProduct: 1, price: 10, description: "A"},
-    {idProduct: 2, price: 10, description: "B"},
-    {idProduct: 3, price: 10, description: "C"}
-];
+const connection = pgp()("postgres://postgres:123456789@localhost:5432/ecommerce");
+//const connection = pgp()("server=localhost;port=5432;database=gso20012023;user id=postgres;password=123456789;");
 
-const coupons = [
-    {code: "VALE20", percentage: 20}
-];
+
+// const products = [
+//     {idProduct: 1, price: 10, description: "A"},
+//     {idProduct: 2, price: 10, description: "B"},
+//     {idProduct: 3, price: 10, description: "C"}
+// ];
+
+// const coupons = [
+//     {code: "VALE20", percentage: 20}
+// ];
 
 app.get("/", (req, res) => {
     res.json({message: 'ok'});
 });
 
-app.post("/checkout", (req, res) => {
+app.post("/checkout", async (req, res) => {
     const cpfIsValid = validate(req.body.cpf);
     if(!cpfIsValid) {
         return res.status(422).json({
@@ -29,7 +34,8 @@ app.post("/checkout", (req, res) => {
     let total = 0;
     const items = req.body.items;
     for(let item of items) {
-        const product = products.find((product) => product.idProduct === item.idProduct);
+        // const product = products.find((product) => product.idProduct === item.idProduct);
+        const [product] = await connection.query(`select * from ecommerce.product where id_product = ${item.idProduct}`);
         if(!product) {
             return res.status(422).json({
                 message: "Product not exist"
@@ -39,7 +45,8 @@ app.post("/checkout", (req, res) => {
     }
 
     if(req.body.coupon) {
-       const coupon = coupons.find((coupon) => coupon.code === req.body.coupon);
+    //    const coupon = coupons.find((coupon) => coupon.code === req.body.coupon);
+        const [coupon] = await connection.query(`select * from ecommerce.coupon where code = '${req.body.coupon}'`);
        if(coupon) {
             total -= (total * coupon.percentage) / 100;
        } 
@@ -51,5 +58,6 @@ app.post("/checkout", (req, res) => {
 });
 
 app.listen(PORT, () => {
+    console.log(connection);
     console.log(`Rodando em ${PORT}`);
 });
